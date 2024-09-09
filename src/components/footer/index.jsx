54a794415a0bedb85e9bb1a052e1afcccebddbf5
Mohadese Arab <mohi.arab97@gmail.com>
2024-09-10 02:21:56 +0430

    footer css complete

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,39 +1,69 @@
-import classes from "./footer.module.scss"
+import classes from "./footer.module.scss";
 
-import logoImg from "../../assets/images/amazon-logo.avif"
+import logoImg from "../../assets/images/amazon-logo.avif";
+import usaFlag from "../../assets/images/usaFlag.jpg";
+import { BsGlobe2 } from "react-icons/bs";
+import { TbCurrencyDollar } from "react-icons/tb";
 
-import data from "../../data.json"
+import data from "../../data.json";
 
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <div className={`${classes.backToTopBtn} d-flex align-center justify-center`}>
+      <div
+        className={`${classes.backToTopBtn} d-flex align-center justify-center`}
+      >
         <p>Back to top</p>
       </div>
 
       <div className={classes.footerContent}>
         {Object.entries(data.footer_data[0]).map((key) => (
-            <ul key={key[0]} className="d-flex flex-column ">
-                <p>{key[0]}</p>
-                {key[1].map((item) => (
-                    <li key={item.id}><a href="#">{item.title}</a></li>
-                ))}
-            </ul>
+          <ul key={key[0]} className="d-flex flex-column ">
+            <p>{key[0]}</p>
+            {key[1].map((item) => (
+              <li key={item.id}>
+                <a href="#">{item.title}</a>
+              </li>
+            ))}
+          </ul>
         ))}
       </div>
 
-      <div className={classes.footerBottom}>
+      <div
+        className={`${classes.footerBottom} d-flex align-center justify-center`}
+      >
         <div className={classes.logo}>
+          <a href="#">
             <img src={logoImg} alt="logo" />
+          </a>
         </div>
-        <div className={classes.infoWrapper}>
-            <div className={classes.lng}>
-                
+        <div
+          className={`${classes.infoWrapper} d-flex align-center justify-center`}
+        >
+          <div
+            className={`${classes.infoItem} d-flex align-center justify-center`}
+          >
+            <BsGlobe2 /> <span>English</span>
+          </div>
+
+          <div
+            className={`${classes.currency} ${classes.infoItem} d-flex align-center justify-center`}
+          >
+            <TbCurrencyDollar className={classes.currencyIcon} />
+            <span> USD - U.s Dollor</span>
+          </div>
+          <div
+            className={`${classes.country} ${classes.infoItem} d-flex align-center justify-center`}
+          >
+            <div className={`${classes.usaFlag}`}>
+              <img src={usaFlag} alt="flag" />
             </div>
+            <span> United States</span>
+          </div>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
